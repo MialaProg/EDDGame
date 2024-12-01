15a0c3830dc82aa307d7904afad6c370500a682e
Miala <miala+gitBdg@ik.me>
2024-12-01 20:10:45 +0100

    disabled

--- gameLib/game.js
+++ gameLib/game.js
@@ -1,20 +1,45 @@
 //Initialisation des variables globales
 var gameStart = false
+var players = new Array(5)
+
+/**
+ * Detect duplicates from an array
+ * @param {array} Array 
+ * @returns {bool}
+ */
+function hasDuplicates(arr) {
+    return arr.some((item, index) => arr.indexOf(item) !== index);
+  }
 
 /**
  * Initialise les actions lors des mises à jour des select
  */
 function init_select_onclick(){
-    // Sélectionner tous les éléments <select> sur la page
+    // Sélectionne tous les éléments <select> sur la page
     var selects = document.querySelectorAll("select");
 
-    // Boucle à travers chaque <select> et définir l'événement onclick
+    // Boucle à travers chaque <select> et définir l'événement change
     selects.forEach(function(select) {
         select.addEventListener("change", function() {
-            console.log("Option selected: " + this.value); // Remplacez cette ligne par votre logique
+            console.log("Option selected: " + this.value);
+
+            //Récupération de l'ID select
+            let selectID = parseInt(this.id[6])
+
+            //Save value
+            players[selectID-1] = this.value
+
+            //Modification des options du select suivant
+            let next_select = document.getElementById('player'+selectID + 1)
+            next_select.querySelectorAll('option').forEach(function(option) {
+                if (!players.includes(option.value)) {
+                    option.disabled = false;
+                }
+            });
         });
     });
 }
+
 
 document.addEventListener("DOMContentLoaded", function() {
     init_select_onclick();
