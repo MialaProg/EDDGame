91b133ca8748429289955847f9115681505f9e03
Miala <65893041+Miala-python@users.noreply.github.com>
2024-12-01 19:17:28 +0100

    wazo

--- gameLib/game.js
+++ gameLib/game.js
@@ -1,2 +1,21 @@
 //Initialisation des variables globales
 var gameStart = false
+
+/**
+ * Initialise les actions lors des mises à jour des select
+ */
+function init_select_onclick(){
+    // Sélectionner tous les éléments <select> sur la page
+    var selects = document.querySelectorAll("select");
+
+    // Boucle à travers chaque <select> et définir l'événement onclick
+    selects.forEach(function(select) {
+        select.addEventListener("change", function() {
+            console.log("Option selected: " + this.value); // Remplacez cette ligne par votre logique
+        });
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    init_select_onclick();
+});
