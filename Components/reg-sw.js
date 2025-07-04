4cbb72cf5dc80c249e1ce3dff1403907b980de9a
Miala <65893041+Miala-python@users.noreply.github.com>
2025-07-04 09:27:31 +0200

    Update reg-sw.js

--- Components/reg-sw.js
+++ Components/reg-sw.js
@@ -10,7 +10,7 @@ if ('serviceWorker' in navigator) {
         ];
         
         // Enregistre le SW avec la liste des assets
-        navigator.serviceWorker.register('./Components/sw.js', {
+        navigator.serviceWorker.register('/sw.js', {
             updateViaCache: 'none',
             scope: '/'
         }).then(registration => {
